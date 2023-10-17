2eb9c90ce416a3501b5bcebe6af410dec2ec88d6
Alice Sze <ys788@cornell.edu>
2023-10-17 19:57:13 -0400

    added mem2reg pass and changed something.c

--- licm/LICM.cpp
+++ licm/LICM.cpp
@@ -5,65 +5,89 @@
 #include "llvm/Analysis/ValueTracking.h"
 #include "llvm/IR/Dominators.h"
 #include "llvm/ADT/SmallVector.h"
+#include "llvm/Transforms/Utils/Mem2Reg.h"
+#include <queue>
+
 using namespace llvm;
-namespace {
-struct LICMPass : public PassInfoMixin<LICMPass> {
-    PreservedAnalyses run (Loop &L, LoopAnalysisManager &AM, LoopStandardAnalysisResults &AR, LPMUpdater &U) {
-        DominatorTree& DT = AR.DT;
-        errs() << "I saw a loop with loop depth " << L.getLoopDepth () << "!\n";
-        for (auto &B : L.blocks()) {
-            for (auto &I: *B) {
-                // TODO: hasLoopInvariantOperands only checks that all reaching defintions of arguments
-                // are outside of the loop and is too conservative. We also want to cover the second criterion "exactly one definition, and it is already marked as
-                // loop invariant"
-                if (!L.hasLoopInvariantOperands(&I)) {
-                    continue;
+namespace
+{
+    struct LICMPass : public PassInfoMixin<LICMPass>
+    {
+        PreservedAnalyses run(Loop &L, LoopAnalysisManager &AM, LoopStandardAnalysisResults &AR, LPMUpdater &U)
+        {
+            DominatorTree &DT = AR.DT;
+            for (auto &B : L.blocks())
+            {
+                std::queue<std::pair<Instruction *, BasicBlock *>> toBeHoisted;
+                for (auto &I : *B)
+                {
+                    // TODO: hasLoopInvariantOperands only checks that all reaching defintions of arguments
+                    // are outside of the loop and is too conservative. We also want to cover the second criterion "exactly one definition, and it is already marked as
+                    // loop invariant"
+                    if (!L.hasLoopInvariantOperands(&I))
+                    {
+                        continue;
+                    }
+                    if (!all_of(I.uses(), [&DT, &I](Use &U)
+                                { return DT.dominates(&I, U); }))
+                        continue;
+                    SmallVector<BasicBlock *> exitBlocks;
+                    L.getExitBlocks(exitBlocks);
+                    if (!all_of(exitBlocks, [&DT, &I](BasicBlock *B)
+                                { return DT.dominates(&I, B); }))
+                        continue;
+
+                    if (isSafeToSpeculativelyExecute(&I))
+                    {
+                        BasicBlock *preheader = L.getLoopPreheader();
+                        auto *PI = &I;
+                        toBeHoisted.push({PI, preheader});
+
+                        errs() << "Hoisted instruction: " << *PI << "\n";
+                    }
                 }
-                // check that def dominates all uses
-                if (!all_of(I.uses(), [&DT, &I](Use &U) { return DT.dominates(&I, U); })) continue;
-                // get loop exits
-                SmallVector<BasicBlock*> exitBlocks;
-                L.getExitBlocks(exitBlocks);
-                // TODO: if instruction does not dominate exits, we still want to try to move it to the preheader
-                // because most instructions don't. See notes for how to relax this restriction
-                // we can use isSafeToSpeculativelyExecute(&I) to check that there are no side effects
-                // check that instruction dominates all loop exits
-                if (!all_of(exitBlocks, [&DT, &I](BasicBlock* B) { return DT.dominates(&I, B); })) continue;
-                
-                // TODO: if all criteria are satisfied, we can move the instruction to the preheader 
-                // (we can get the preheader via L.getLoopPreheader())
-                if (isSafeToSpeculativelyExecute(&I)) {
-                    BasicBlock *preheader = L.getLoopPreheader();
-                    Instruction *PT = preheader->getTerminator(); // Insert before the terminator of the preheader
-                    auto *PI = &I;
-                    // Insert the instruction into the preheader's terminator
-                    // TODO: Figure out why this is breaking here
-                    // It may be due to I being in a different basic block from PT
-                    // PI->removeFromParent();
-                    // PI->insertBefore(PT);
-                    errs() << "Hoisted instruction: " << *PI << "\n";
+                while (!toBeHoisted.empty())
+                {
+                    const auto &[I, preheader] = toBeHoisted.front();
+                    toBeHoisted.pop();
+                    I->removeFromParent();
+                    I->insertInto(preheader, preheader->end());
                 }
-                // errs() <<  "Potentially hoistable instruction: " << I << "\n";
-            } 
-        }
-        
-        return PreservedAnalyses::all();
+            }
+
+            return PreservedAnalyses::none();
+        };
+        static bool isRequired() { return true; }
     };
-    static bool isRequired() { return true; }
-};
 };
 
 extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
-llvmGetPassPluginInfo() {
+llvmGetPassPluginInfo()
+{
     return {
         .APIVersion = LLVM_PLUGIN_API_VERSION,
         .PluginName = "Skeleton pass",
         .PluginVersion = LLVM_VERSION_STRING,
-        .RegisterPassBuilderCallbacks = [](PassBuilder &PB) {
-            PB.registerLateLoopOptimizationsEPCallback(
-                [](llvm::LoopPassManager &LPM, OptimizationLevel Level) {
-                  LPM.addPass(LICMPass());
+        .RegisterPassBuilderCallbacks = [](PassBuilder &PB)
+        {
+            PB.registerScalarOptimizerLateEPCallback(
+                [](FunctionPassManager &FPM, OptimizationLevel Level) {
+                    FPM.addPass(PromotePass());
+                    FPM.addPass(createFunctionToLoopPassAdaptor(LICMPass()));
                 });
-        }
-    };
+
+            // use the following code if you want to run the passes with opt    
+ 
+            // PB.registerPipelineParsingCallback(
+            //     [](StringRef Name, LoopPassManager &LPM,
+            //        ArrayRef<PassBuilder::PipelineElement>)
+            //     {
+            //         if (Name == "licm-pass") {
+            //             LPM.addPass(createFunctionToLoopPassAdaptor(LICMPass()));
+            //             return true;
+            //         }
+            //         return false;
+                    
+            //     });
+        }};
 }
--- something.c
+++ something.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
 
-int main() {
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 10; j++) {
-            int k = 0;
+int f(int arg) {
+    int l = arg;
+
+        for (int j = 0; j < arg; j++) {
+
+            int k = 8;
+            l = k;
         }
-    }
-    return 0;
+    return l;
 }
+
+int main() {
+    int l = 29;
+    // for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 10; j++) {
+
+            int k = 8;
+            l = k;
+        }
+    // }
+    printf("The value of the number is: %d\n", l);
+    
+    return f(l);
+}
+
+
 
